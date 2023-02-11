d0d6d21f3ff05238b2e0e95ba628474ce59938f6
AnthonyPierard <57867046+AnthonyPierard@users.noreply.github.com>
2023-02-11 10:49:59 +0100

    suite exemple django
    je suis à la lettre le tutoriel proposé par django

--- CollegeBookSite/CollegeBookSite/settings.py
+++ CollegeBookSite/CollegeBookSite/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #pour le test
+    'polls.apps.PollsConfig'
 ]
 
 MIDDLEWARE = [
@@ -72,7 +74,7 @@ WSGI_APPLICATION = 'CollegeBookSite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+#à changer pour une base de donnée mySQL ou autre.
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
