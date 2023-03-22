278afaeabe5f02199b38dc76a682534f1e541182
AnthonyPierard <57867046+AnthonyPierard@users.noreply.github.com>
2023-03-22 13:35:55 +0100

    ajout des préfixe de json
    pour changer de config de salle il faut changer l'URL dans le fichier js

--- src/Configuration/static/js/Configuration.js
+++ src/Configuration/static/js/Configuration.js
@@ -6,21 +6,33 @@ seat.addEventListener('click', () => {
     menu.classList.toggle('clicked') // créer une classe à un élément et la retire si on reclique
 })*/
 
-function fill_seat(seats){
+function fill_seat(obj){
     const seat_area = document.querySelector('.seat-area');
-    const row = document.createElement('div');
-    row.class = seats.class;
-    seat_area.appendChild(row);
+    for (const index in obj){
+        const row = document.createElement('div');
+        row.classList.add(obj[index].class);
+        seat_area.appendChild(row);
+        if(obj[index].seat != null){
+               const all_seat = obj[index].seat;
 
-    for (const seat of seats.seat){
-        const balise_seat = document.createElement('div');
-        console.log(seat);
+            for (const seat of all_seat){
+                const balise_seat = document.createElement('div');
+                const new_seat = seat.split(' ');
+                for (const class_seat of new_seat) {
+                    console.log(class_seat);
+                    balise_seat.classList.add(class_seat);
+                }
+                row.appendChild(balise_seat);
+            }
+        }
+
     }
+
 
 }
 async function prepare_json() {
 
-    const requestURL = 'https://github.com/AnthonyPierard/CollegeBook/blob/Refactoring/src/Configuration/static/json/allSeat.json'; //je ne sais pas comment y accéder
+    const requestURL = "../static/json/onlySeat.json" //'https://github.com/AnthonyPierard/CollegeBook/blob/Refactoring/src/Configuration/static/json/allSeat.json'; //je ne sais pas comment y accéder
     const request = new Request(requestURL);
     const response = await fetch(request);
     const seat = await response.json();
