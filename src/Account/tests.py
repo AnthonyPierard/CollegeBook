2b9c97d905e6add41c82db83aa36619442dd3073
emilejanssens <emilejm@gmail.com>
2023-03-23 09:55:43 +0100

    test delete et update representation et test details event

--- src/Account/tests.py
+++ src/Account/tests.py
@@ -1,15 +1,11 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-from webdriver_manager.chrome import ChromeDriverManager
-from time import sleep
+from CollegeBook.tests import BaseTest
 from .models import User
 
-from CollegeBook.tests import BaseTest
+
 class AccountTest(StaticLiveServerTestCase):
     @staticmethod
     def init_superuser():
@@ -59,7 +55,7 @@ class AccountTest(StaticLiveServerTestCase):
         driver = BaseTest.init(self)
         AccountTest.init_superuser()
         AccountTest.login_superuser(driver)
-        link = driver.find_element(By.ID,'nav-logout')
+        link = driver.find_element(By.ID, 'nav-logout')
         link.click()
         assert 'Se connecter' in driver.page_source
         driver.close()
@@ -107,7 +103,7 @@ class AccountTest(StaticLiveServerTestCase):
         driver = BaseTest.init(self)
         AccountTest.init_user()
         AccountTest.login_user(driver)
-        link = driver.find_element(By.ID,'nav-personnal-events')
+        link = driver.find_element(By.ID, 'nav-personnal-events')
         link.click()
         assert 'Tous vos événements' in driver.page_source
         driver.close()
@@ -116,19 +112,19 @@ class AccountTest(StaticLiveServerTestCase):
         driver = BaseTest.init(self)
         AccountTest.init_superuser()
         AccountTest.login_superuser(driver)
-        link = driver.find_element(By.ID,'nav-create-account')
+        link = driver.find_element(By.ID, 'nav-create-account')
         link.click()
-        box_first_name = driver.find_element(By.NAME,'first_name')
-        box_last_name = driver.find_element(By.NAME,'last_name')
-        box_email = driver.find_element(By.NAME,'email')
-        box_password = driver.find_element(By.NAME,'password')
+        box_first_name = driver.find_element(By.NAME, 'first_name')
+        box_last_name = driver.find_element(By.NAME, 'last_name')
+        box_email = driver.find_element(By.NAME, 'email')
+        box_password = driver.find_element(By.NAME, 'password')
         box_first_name.send_keys('test first')
         box_last_name.send_keys('test last')
         box_email.send_keys('test@email.com')
         box_password.send_keys('testpassword')
-        box_submit = driver.find_element(By.CSS_SELECTOR,"input[type='submit']")
+        box_submit = driver.find_element(By.CSS_SELECTOR, "input[type='submit']")
         box_submit.send_keys(Keys.RETURN)
-        link = driver.find_element(By.ID,'nav-admins')
+        link = driver.find_element(By.ID, 'nav-admins')
         link.click()
         assert 'Nom et prénom' in driver.page_source
         driver.close()
--- src/CollegeBook/tests.py
+++ src/CollegeBook/tests.py
@@ -2,8 +2,6 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
 
