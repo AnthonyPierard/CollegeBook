c7a917d9b7d25d75f28e5e648b77485590a071db
AnthonyPierard <57867046+AnthonyPierard@users.noreply.github.com>
2023-03-28 11:33:08 +0200

    json in bd and new button

--- src/Configuration/models.py
+++ src/Configuration/models.py
@@ -6,8 +6,9 @@ from Account.models import User
 class Config(models.Model):
     name = models.CharField('Nom de configuration', max_length=30, blank=True, unique=True)
     seating_arrangement = models.JSONField("Disposition de la salle", blank=True, null=True) #TODO retirer le blank ensuite mais pour teste ici
+    url_json = models.CharField('URL du fichier json', max_length=100, blank=True)
 
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
-        return self.config_nom
+        return self.name
--- src/Configuration/views.py
+++ src/Configuration/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from .models import Config
 
 @login_required
 def area_configuration(request):
-    return render(request, 'area_configuration.html')
+    configs = Config.objects.filter(user=request.user.id)
+    return render(request, 'area_configuration.html', {'configs' : configs})
