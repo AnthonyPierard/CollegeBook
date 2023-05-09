0b5565a9e82290859dbbf34eecab6518f64e1eae
Lucas Berg <55436804+BergLucas@users.noreply.github.com>
2023-05-09 14:36:44 +0200

    Add migrations

--- src/restore_BD.sh
+++ src/restore_BD.sh
@@ -5,7 +5,6 @@ python ./manage.py makemigrations Event
 python ./manage.py makemigrations Reservation
 python ./manage.py migrate
 python ./manage.py shell -c "from Account.models import User; User.objects.create_superuser('admin@admin.com', 'adminpassword')"
-find -type d -name migrations -a -prune -exec rm -rf {} \;
 find -type d -name __pycache__ -a -prune -exec rm -rf {} \;
 rm -rf Event/static/json/*
 rm -rf CollegeBook/Media/QR/*
